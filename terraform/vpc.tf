fca8376af86625dd691585bea688b126ad07f315
Maxim Generalov <generalov.maksim@gmail.com>
2023-12-04 20:30:39 +0000

    test depends on

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,9 +4,6 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "subnetwork" {
-  depends_on = [
-    google_compute_network.vpc_network
-  ]
   name                     = var.vpc.subnetwork
   ip_cidr_range            = "10.0.0.0/24"
   region                   = var.gcp.region
